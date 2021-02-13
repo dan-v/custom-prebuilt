515044a4626052d2988fd3709d8b6ddf1e94a575
Dan Vittegleo <dan@vittegleo.com>
2021-02-12 19:36:34 -0800

    testing

--- vendor/config/main.mk
+++ vendor/config/main.mk
@@ -8,4 +8,4 @@ PRODUCT_PACKAGES += \
     TrichromeWebView
 
 PRODUCT_COPY_FILES += \
-	vendor/custom/vendor/prebuilt/google_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/google_experience.xml
+	vendor/custom/prebuilt/google_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/google_experience.xml
