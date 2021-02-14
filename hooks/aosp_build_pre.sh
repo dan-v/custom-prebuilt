8f5756394203ca6b39cb799289352f881d4ca747
Dan Vittegleo <dan@vittegleo.com>
2021-02-14 12:26:38 -0800

    testing

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-log "Applying MicroG sigspoof patch"
+# add microg sigspoof patch. microg also requires adding microg prebuilts in vendor/config/main.mk
+log "applying microg sigspoof patch"
 pushd "${AOSP_BUILD_DIR}"
 patch -p1 --no-backup-if-mismatch < "platform/prebuilts/microg/00002-microg-sigspoof.patch"
+popd
+
+# apply community patches
+log "applying community patch 00001-global-internet-permission-toggle.patch"
+git clone https://github.com/RattlesnakeOS/community_patches "${ROOT_DIR}/community_patches"
+pushd "${AOSP_BUILD_DIR}"
+patch -p1 --no-backup-if-mismatch < "${ROOT_DIR}/community_patches/00001-global-internet-permission-toggle.patch"
 popd
--- hooks/aosp_local_repo_additions_post.sh
+++ hooks/aosp_local_repo_additions_post.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# log "Removing local RattlesnakeOS Chromium manifest"
-# rm -f "${AOSP_BUILD_DIR}/.repo/local_manifests/001-chromium.xml"
+# if not building chromium, remove default chromium manifest.
+if [ "${CHROMIUM_BUILD_DISABLED}" == "true" ]; then
+    log "removing local manifest 001-chromium.xml"
+    rm -f "${AOSP_BUILD_DIR}/.repo/local_manifests/001-chromium.xml"
+fi
